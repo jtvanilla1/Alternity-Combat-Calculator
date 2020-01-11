3bad9047430127832d24fc0fbd1baee979aafa57
Josiah Manning <46797864+jtvanilla1@users.noreply.github.com>
2020-01-10 17:35:13 -0800

    Add files via upload
    initial upload

---
+++ src/alternitymodifiercalculator/ConstantCodes.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alternitymodifiercalculator;
+
+/**
+ *
+ * @author josia
+ */
+public class ConstantCodes {
+    public static int TotalRangedModifier = 0;
+    public static int TotalMeleeModifier = 0;
+    public static int WeaponChoice = 1;
+    public static boolean flag1 = false, flag2 = false, flag3=false, flag4=false;
+    public static int prevNum = 0;
+    public static int TotalOtherModifiersR = 0;
+    public static int TotalOtherModifiersM = 0;
+}
